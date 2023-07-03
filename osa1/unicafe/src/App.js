c73612b7ed5aff390fe7a8b27cc38083ba610dd2
Kari-Pekka Ahola <karipekka.ahola@protonmail.com>
2023-07-03 10:40:42 +0300

    1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,10 +1,24 @@
 import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  return (
+    <>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good - bad) / all}</p>
+      <p>positive {good / all * 100} %</p>
+    </>
+  )
+}
+
 const App = () =>  {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
 
   const handleClickGood = () => setGood(good + 1)
   const handleClickNeutral = () => setNeutral(neutral + 1)
@@ -16,13 +30,7 @@ const App = () =>  {
       <button onClick={handleClickGood}>good</button>
       <button onClick={handleClickNeutral}>neutral</button>
       <button onClick={handleClickBad}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good - bad) / all}</p>
-      <p>positive {good / all * 100} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad} / >
     </div>
   )
 }
