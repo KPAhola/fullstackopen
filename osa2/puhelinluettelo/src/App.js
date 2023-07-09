18ea6ea5bf41b38cc6d0ff93c901d9ced50797bc
Kari-Pekka Ahola <karipekka.ahola@protonmail.com>
2023-07-09 15:40:16 +0300

    2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,21 +2,31 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    {
+      name: 'Arto Hellas',
+      number: '040-1231244'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addContact = (event) => {
     event.preventDefault()
     const newContact = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     persons.map(person => person.name).includes(newName) ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat(newContact))
     setNewName('')
+    setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -30,11 +40,17 @@ const App = () => {
                 />
         </div>
         <div>
+          number: <input 
+                  value={newNumber}
+                  onChange={handleNumberChange}
+                />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 
