bdb5608511fe4f24bd11fbf64761fb8fd0242879
Kari-Pekka Ahola <karipekka.ahola@protonmail.com>
2023-07-25 21:07:10 +0300

    3.5

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     "id": 1,
@@ -50,6 +52,23 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
+})
+
+const generateId = () => {
+  const ids = persons.map(person => person.id)
+  const id = Math.floor(Math.random() * 10000000) + 1
+  if (!(ids.includes(id))) {
+    return id
+  } else {
+    return generateId()
+  }
+}
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  person.id = generateId()
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
