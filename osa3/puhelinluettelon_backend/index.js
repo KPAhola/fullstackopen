880c16904740386ae650b3d7003494dde7e52e81
Kari-Pekka Ahola <karipekka.ahola@protonmail.com>
2023-08-05 20:45:16 +0300

    3.16

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -57,9 +57,10 @@ app.get('/api/persons/:id', (request, response) => {
     : response.status(404).end()
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => response.status(204).end())
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -79,6 +80,21 @@ app.post('/api/persons', (request, response) => {
     })
   }
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
